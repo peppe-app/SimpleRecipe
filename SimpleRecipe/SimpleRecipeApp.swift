34aa60b20330271c1337c88446fd5d2bda01d26b
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-02-29 08:49:22 +1100

    Initial Commit

---
+++ SimpleRecipe/SimpleRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeApp.swift
+//  SimpleRecipe
+//
+//  Created by Giuseppe Sapienza on 29/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
